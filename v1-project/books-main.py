1c753aa914f8bcf89beb685aecad22c5dbb724d2
Your Name <you@example.com>
2023-01-22 19:25:23 -0300

    Implemented POST request to add new books to our books dictionary. The books dictionary resets as soon as we restart the server.

--- v1-project/books-main.py
+++ v1-project/books-main.py
@@ -2,10 +2,33 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def first_api():
-    return {"message": "Hello Lucas!"}
+book_list = {
+    'book_1': {'title': 'title one', 'author': 'author one'},
+    'book_2': {'title': 'title two', 'author': 'author two'},
+    'book_3': {'title': 'title three', 'author': 'author three'},
+    'book_4': {'title': 'title four', 'author': 'author four'},
+    'book_5': {'title': 'title five', 'author': 'author five'}
+}
 
-@app.get("/hola")
-async def first_api():
-    return {"message": "Hello Maria!"} 
+@app.get("/")
+async def read_all_books():
+    return book_list
+
+@app.get("/books/{book_name}")
+async def read_book(book_name: str):
+    return book_list[book_name]
+
+@app.post("/")
+async def create_book(book_title, book_author):
+    curr_book_id = 0
+    if(len(book_list) > 0):
+        # Search the max number of book id in the list. We do the for in case the books are not ordered
+        for book in book_list:
+            x = int(book.split('_')[-1])
+            if(x > curr_book_id):
+                print(curr_book_id)
+                curr_book_id = x
+    new_id = "book_" + str(curr_book_id + 1)
+    book_list[new_id] = {'title': book_title, 'author': book_author}
+    return book_list
+        
